427fd1573aaf496968ca3d5f4fc17317b9876c18
38558 <38558@0338S-S-R0B6ZHP.sdcs.local>
2019-04-30 14:23:53 -0700

    progress

---
+++ src/houses/Houses.java
@@ -0,0 +1,27 @@
+package houses;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class Houses {
+
+	public static void main(String[] args) {
+	  Robot r2d2=new Robot();
+	  r2d2.miniaturize();
+	  r2d2.moveTo(50,450);
+	  r2d2.penDown();
+	  r2d2.move(100);
+	  r2d2.turn(45);
+	  r2d2.move(25);
+	  r2d2.turn(90);
+	  r2d2.move(25);
+	  r2d2.turn(45);
+	  r2d2.move(100);
+	  r2d2.turn(90);
+	 
+	
+	
+	
+	
+	
+}
+}
