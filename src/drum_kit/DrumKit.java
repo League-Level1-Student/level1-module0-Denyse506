a52c45ec2611c707fc77f9005bcd4913f1a862e7
38558 <38558@0338S-Z-90TDE0N.sdcs.local>
2019-11-19 14:21:12 -0800

    Almost done

--- src/drum_kit/DrumKit.java
+++ src/drum_kit/DrumKit.java
@@ -28,27 +28,28 @@ public class DrumKit implements MouseListener {
 JFrame frame= new JFrame();
 		// 2. Make the frame visible and
 		// set its default close operation to JFrame.EXIT_ON_CLOSE
-JFrame.EXIT_ON_CLOSE
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// 3. Set the size of the frame
-
+frame.setSize(null);
 		// 4. Set the title of the frame
-
+frame.setTitle(null);
 		// 5. Make a JPanel variable and initialize it using "new JPanel().
+JPanel panel= new JPanel();
 
 		// 6. Add the panel to the frame. (The panel is invisible.)
-
+frame.add(panel);
 		// 7. Download an image of a drum from the Internet. Drop it into your
 		// Eclipse project under "default package".
 
 		// 8. Put the name of your image file in a String variable.
-
+String imageString = "src/drum_kit/drumline.png";
 		// 9. Edit the next line to use your String variable
-		// drumLabelWithImage = createLabelImage(drumImageString);
+		drumLabelWithImage = createLabelImage(imageString);
 
 		// 10. Add the image to the panel
-
+       panel.add(drumLabelWithImage);
 		// 11. Set the layout of the panel to "new GridLayout()"
-
+     panel.setLayout(new GridLayout());
 		// 12. call the pack() method on the frame. Run your program. Do you see
 		// your drum image?
 
